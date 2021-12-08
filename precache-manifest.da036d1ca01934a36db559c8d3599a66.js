e77ca218ebc54ba890850abc063c092ded0eb0f7
Michelle Wingter <mwingter@gmail.com>
2021-12-07 21:33:10 -0300

    Updates

---
+++ precache-manifest.da036d1ca01934a36db559c8d3599a66.js
@@ -0,0 +1,382 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7daf48019656063ebab4597197479e",
+    "url": "/semcomp-a11y/index.html"
+  },
+  {
+    "revision": "20aa2e59f16df7666b43",
+    "url": "/semcomp-a11y/static/css/2.b9edca59.chunk.css"
+  },
+  {
+    "revision": "997f70d41c7e9551cdb3",
+    "url": "/semcomp-a11y/static/css/main.99d7be5b.chunk.css"
+  },
+  {
+    "revision": "20aa2e59f16df7666b43",
+    "url": "/semcomp-a11y/static/js/2.309d6dc9.chunk.js"
+  },
+  {
+    "revision": "5a46a751d105f4460f53ba834dcfca3f",
+    "url": "/semcomp-a11y/static/js/2.309d6dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997f70d41c7e9551cdb3",
+    "url": "/semcomp-a11y/static/js/main.b0cdad89.chunk.js"
+  },
+  {
+    "revision": "d27e57687b5b1f556b24",
+    "url": "/semcomp-a11y/static/js/runtime-main.f48963a6.js"
+  },
+  {
+    "revision": "27b266bf8598fc9af02d4ffd6df12392",
+    "url": "/semcomp-a11y/static/media/Staatliches-Regular.27b266bf.eot"
+  },
+  {
+    "revision": "7794533cbddbc0f694f9b35acab505aa",
+    "url": "/semcomp-a11y/static/media/Staatliches-Regular.7794533c.woff"
+  },
+  {
+    "revision": "d5746e3b20453d6cee99ab27a0bdf949",
+    "url": "/semcomp-a11y/static/media/Staatliches-Regular.d5746e3b.ttf"
+  },
+  {
+    "revision": "70c1f283666376cc31522368c73357c6",
+    "url": "/semcomp-a11y/static/media/a_uniao_faz_a_forca.70c1f283.png"
+  },
+  {
+    "revision": "1822a740b47ddb638b4671966bfda5c4",
+    "url": "/semcomp-a11y/static/media/agamotto.1822a740.jpg"
+  },
+  {
+    "revision": "0e4b62567dc51c31a5df2a5ab9072e31",
+    "url": "/semcomp-a11y/static/media/alongados.0e4b6256.png"
+  },
+  {
+    "revision": "8c8bece94fc3f5a6598dcf4397854502",
+    "url": "/semcomp-a11y/static/media/amdocs_logo.8c8bece9.svg"
+  },
+  {
+    "revision": "38baafb14a3b15002b0e40af5e463660",
+    "url": "/semcomp-a11y/static/media/americanas_logo.38baafb1.svg"
+  },
+  {
+    "revision": "5cfc667b5ca860ea99c8dc7283f97e10",
+    "url": "/semcomp-a11y/static/media/aqui_e_o_brasil.5cfc667b.png"
+  },
+  {
+    "revision": "c2b1439aec756e124247a80014dcfcb2",
+    "url": "/semcomp-a11y/static/media/arquivei_logo.c2b1439a.svg"
+  },
+  {
+    "revision": "32d920fd49f68e87309904a855acc767",
+    "url": "/semcomp-a11y/static/media/banner.32d920fd.png"
+  },
+  {
+    "revision": "cfc680216378b6090cf2769b17ca0f11",
+    "url": "/semcomp-a11y/static/media/bill_gates.cfc68021.png"
+  },
+  {
+    "revision": "643badfde1387fd02984721e3a22e4fe",
+    "url": "/semcomp-a11y/static/media/biopresente.643badfd.png"
+  },
+  {
+    "revision": "b19eb2d9f19df15bb492a30de70e8f5e",
+    "url": "/semcomp-a11y/static/media/buscando_conhecimento.b19eb2d9.png"
+  },
+  {
+    "revision": "2080e0abda734bbe4fa04fb2500513c6",
+    "url": "/semcomp-a11y/static/media/casa_dos_designers.2080e0ab.png"
+  },
+  {
+    "revision": "e78bfd91c26bffcf029bd223249d939e",
+    "url": "/semcomp-a11y/static/media/casa_espacial.e78bfd91.png"
+  },
+  {
+    "revision": "bed54e5e9f2c4e8c7f95610bc1af773c",
+    "url": "/semcomp-a11y/static/media/chave_de_jade.bed54e5e.png"
+  },
+  {
+    "revision": "18cb779fc8e95ef29cb1d18cca7999f7",
+    "url": "/semcomp-a11y/static/media/clara_e_gema.18cb779f.png"
+  },
+  {
+    "revision": "d2e1c240f11b03383e0c9eb2444b1561",
+    "url": "/semcomp-a11y/static/media/cultista.d2e1c240.png"
+  },
+  {
+    "revision": "557bf396ceb524f0494d2f3616d8ac1a",
+    "url": "/semcomp-a11y/static/media/ddd.557bf396.png"
+  },
+  {
+    "revision": "91bd5e72c4cdb76fb23ff611757dfdc7",
+    "url": "/semcomp-a11y/static/media/delorean.91bd5e72.jpg"
+  },
+  {
+    "revision": "9b5f8d2aed96f73ea75c685122d655ef",
+    "url": "/semcomp-a11y/static/media/desenvolvedores_de_jogos.9b5f8d2a.png"
+  },
+  {
+    "revision": "93338fc6954524f0f7b2e670d6f6e8b9",
+    "url": "/semcomp-a11y/static/media/egg_hunter.93338fc6.png"
+  },
+  {
+    "revision": "f429bad07ff6f757d8fc1ffe1b99d251",
+    "url": "/semcomp-a11y/static/media/elo7_logo.f429bad0.svg"
+  },
+  {
+    "revision": "e9a03371f63e1f926f83ac134bab5fb8",
+    "url": "/semcomp-a11y/static/media/enigmatico.e9a03371.png"
+  },
+  {
+    "revision": "052e2b369273c504d82a8e72207f4f54",
+    "url": "/semcomp-a11y/static/media/escritores_tecnicos.052e2b36.png"
+  },
+  {
+    "revision": "6cf0efb1adf9b28d9e21587368afb911",
+    "url": "/semcomp-a11y/static/media/fa_clube_do_batista.6cf0efb1.png"
+  },
+  {
+    "revision": "553809e64422c5b7b71dd10a5476396e",
+    "url": "/semcomp-a11y/static/media/facebook-logo.553809e6.svg"
+  },
+  {
+    "revision": "1dbecc418520ace1f312e034a2ff295d",
+    "url": "/semcomp-a11y/static/media/familia_do_futuro.1dbecc41.png"
+  },
+  {
+    "revision": "78789d78556abc6bced851ca336f977f",
+    "url": "/semcomp-a11y/static/media/figurante.78789d78.png"
+  },
+  {
+    "revision": "6e39aa41114ba6f18caaf2973420ea2d",
+    "url": "/semcomp-a11y/static/media/fog.6e39aa41.png"
+  },
+  {
+    "revision": "19b9081bb187c788554a2b75906c880c",
+    "url": "/semcomp-a11y/static/media/footer-background.19b9081b.png"
+  },
+  {
+    "revision": "918e1a822c0cc609c28608a441fd1f88",
+    "url": "/semcomp-a11y/static/media/gamehouse.918e1a82.png"
+  },
+  {
+    "revision": "16cef8f0305cc34a214285c9de72f6a0",
+    "url": "/semcomp-a11y/static/media/gamer.16cef8f0.png"
+  },
+  {
+    "revision": "7d9a5517cd2d79126af7b1c50be9199b",
+    "url": "/semcomp-a11y/static/media/gamer_olimpico.7d9a5517.png"
+  },
+  {
+    "revision": "2261e29db004ac70a264315f1e05e462",
+    "url": "/semcomp-a11y/static/media/gema.2261e29d.png"
+  },
+  {
+    "revision": "f677f582dc60fe719169825707d5d8b9",
+    "url": "/semcomp-a11y/static/media/globe-icon.f677f582.svg"
+  },
+  {
+    "revision": "49a79625f85cd0c68e1c7f4fe0ce0965",
+    "url": "/semcomp-a11y/static/media/globo_logo.49a79625.svg"
+  },
+  {
+    "revision": "61897e658164430aeb39c452ddef0d11",
+    "url": "/semcomp-a11y/static/media/habilidade_quebrar_codigos.61897e65.png"
+  },
+  {
+    "revision": "486aa0b3571201b8d3a84c28976946da",
+    "url": "/semcomp-a11y/static/media/hackmaniaco.486aa0b3.png"
+  },
+  {
+    "revision": "a7bb65c3914cf38dedce7a13bd5d19f0",
+    "url": "/semcomp-a11y/static/media/home-background-empty.a7bb65c3.png"
+  },
+  {
+    "revision": "4d1c15f3901ec8e18e9a09e2949eb81b",
+    "url": "/semcomp-a11y/static/media/home-background-mobile.4d1c15f3.png"
+  },
+  {
+    "revision": "684fadba1a5df36c0d6a4caed140d4be",
+    "url": "/semcomp-a11y/static/media/home-background.684fadba.png"
+  },
+  {
+    "revision": "a0801c78e0b5f90732bdac17df3e13f0",
+    "url": "/semcomp-a11y/static/media/icmc.a0801c78.png"
+  },
+  {
+    "revision": "d0871f7bf64f0cd484bf640e24cb187d",
+    "url": "/semcomp-a11y/static/media/ill_be_back.d0871f7b.png"
+  },
+  {
+    "revision": "165f649fad7d66d8c95ebfdbfbd1952f",
+    "url": "/semcomp-a11y/static/media/infosec.165f649f.png"
+  },
+  {
+    "revision": "653e459d26fc5d9d1e01111995eb4eee",
+    "url": "/semcomp-a11y/static/media/instagram-logo.653e459d.svg"
+  },
+  {
+    "revision": "b68b2b1ae10c4c607567279148fdad0f",
+    "url": "/semcomp-a11y/static/media/jogador_n1.b68b2b1a.png"
+  },
+  {
+    "revision": "050dde787b21f16f6870004f95dfed3e",
+    "url": "/semcomp-a11y/static/media/junior.050dde78.png"
+  },
+  {
+    "revision": "de2b73c879158f9b4914edfcf57d8697",
+    "url": "/semcomp-a11y/static/media/lider_temporal.de2b73c8.png"
+  },
+  {
+    "revision": "d5743f8ee456309453239d152a1aa3f0",
+    "url": "/semcomp-a11y/static/media/linkedin-logo.d5743f8e.svg"
+  },
+  {
+    "revision": "620c9ebdfaaff460ae86a0e86bbf78f7",
+    "url": "/semcomp-a11y/static/media/logo-24.620c9ebd.png"
+  },
+  {
+    "revision": "1446b6cfacc5db8ea4eb3647a32a6273",
+    "url": "/semcomp-a11y/static/media/luizalabs_logo.1446b6cf.svg"
+  },
+  {
+    "revision": "4e177d102b72098f6037cfcc5fd84642",
+    "url": "/semcomp-a11y/static/media/marketero.4e177d10.png"
+  },
+  {
+    "revision": "3d4d8b8f1db5e257e77af51dfff2a20f",
+    "url": "/semcomp-a11y/static/media/me_vejo_obrigado_a_concordar_com_o_palestrinha.3d4d8b8f.png"
+  },
+  {
+    "revision": "e6dda56839794965f464fe59237c8f45",
+    "url": "/semcomp-a11y/static/media/mestres_temporais.e6dda568.png"
+  },
+  {
+    "revision": "8d3a349351f706eb7a1d284bcddffc89",
+    "url": "/semcomp-a11y/static/media/micro_presenca.8d3a3493.png"
+  },
+  {
+    "revision": "faa234c33a37829409ef06689c262c71",
+    "url": "/semcomp-a11y/static/media/monitora_logo.faa234c3.svg"
+  },
+  {
+    "revision": "db97672698f7a5819aee3c4d98172279",
+    "url": "/semcomp-a11y/static/media/nao_se_faz_omelete_sem_quebrar_ovos.db976726.png"
+  },
+  {
+    "revision": "f29bc81d976655483563424908dcbbcc",
+    "url": "/semcomp-a11y/static/media/navbar-background.f29bc81d.png"
+  },
+  {
+    "revision": "2f62084862ebad1aff950e4c78014248",
+    "url": "/semcomp-a11y/static/media/netbr_logo.2f620848.svg"
+  },
+  {
+    "revision": "1f66b132712f7598f9f60f592feb88b3",
+    "url": "/semcomp-a11y/static/media/o_clone.1f66b132.png"
+  },
+  {
+    "revision": "6256ab49e3fdc3a70b995c0298691d67",
+    "url": "/semcomp-a11y/static/media/o_culto.6256ab49.png"
+  },
+  {
+    "revision": "af59c64c99da49ae4b3680b0878cfda1",
+    "url": "/semcomp-a11y/static/media/obrigado_pelos_peixes.af59c64c.png"
+  },
+  {
+    "revision": "d8a97f884a4d7e70bdd25a47d0cf17ed",
+    "url": "/semcomp-a11y/static/media/ocarina.d8a97f88.jpg"
+  },
+  {
+    "revision": "f95f69887b81572c96218ae78fedb41d",
+    "url": "/semcomp-a11y/static/media/oloco_meu_o_cara_e_fera.f95f6988.png"
+  },
+  {
+    "revision": "fff85120f9df48006f0ab4e771150117",
+    "url": "/semcomp-a11y/static/media/opus_logo.fff85120.svg"
+  },
+  {
+    "revision": "b217c86fc0822e7fd8f6f7583e448706",
+    "url": "/semcomp-a11y/static/media/por_dentro_das_empresas.b217c86f.png"
+  },
+  {
+    "revision": "a6f7a8c9bc0051733832bb3b2f0e7095",
+    "url": "/semcomp-a11y/static/media/produteiros.a6f7a8c9.png"
+  },
+  {
+    "revision": "f304611dfa0358c96d93faaa3f2c6509",
+    "url": "/semcomp-a11y/static/media/quase_arquitetos.f304611d.png"
+  },
+  {
+    "revision": "b686b4b713d70b3390de3b5cbf9120cc",
+    "url": "/semcomp-a11y/static/media/reclames_do_plim_plim.b686b4b7.png"
+  },
+  {
+    "revision": "b65b9884064ef45ea743dfb0ffd6016b",
+    "url": "/semcomp-a11y/static/media/reforcos_positivo.b65b9884.png"
+  },
+  {
+    "revision": "e7cc7acfe747e0e2a8a158de63265635",
+    "url": "/semcomp-a11y/static/media/reino_do_cospobre.e7cc7acf.png"
+  },
+  {
+    "revision": "e67aa9d67fef890bc366f24e40501221",
+    "url": "/semcomp-a11y/static/media/rinha_de_robos.e67aa9d6.png"
+  },
+  {
+    "revision": "180eb352625d0d031d5bafceff33bbbc",
+    "url": "/semcomp-a11y/static/media/roubo_de_identidade.180eb352.png"
+  },
+  {
+    "revision": "744f38e774ca84002ecb5b5bf429678f",
+    "url": "/semcomp-a11y/static/media/saindo_na_frente.744f38e7.png"
+  },
+  {
+    "revision": "1f5032db67fda5aad72825c7b97d0acf",
+    "url": "/semcomp-a11y/static/media/sem_mais_viagens.1f5032db.png"
+  },
+  {
+    "revision": "6b1c2cd54d575cc9602f019867a4ab93",
+    "url": "/semcomp-a11y/static/media/sem_segredos.6b1c2cd5.png"
+  },
+  {
+    "revision": "6bc3072d66bdd5c41fe42ec226e56d8b",
+    "url": "/semcomp-a11y/static/media/sempre_solicito.6bc3072d.png"
+  },
+  {
+    "revision": "fe4667128b7dc62bded72c3d594266d4",
+    "url": "/semcomp-a11y/static/media/sociedade_secreta.fe466712.png"
+  },
+  {
+    "revision": "918d1fa2e426d21c918dc422e4134db0",
+    "url": "/semcomp-a11y/static/media/tardis.918d1fa2.jpg"
+  },
+  {
+    "revision": "deb625fc0ec19255153bcc5642a54c4b",
+    "url": "/semcomp-a11y/static/media/thats_all_folks.deb625fc.png"
+  },
+  {
+    "revision": "baf23a894377233e8f8f743c8ce43737",
+    "url": "/semcomp-a11y/static/media/tokenlab_logo.baf23a89.svg"
+  },
+  {
+    "revision": "f3a7203cbe804cba33be159c0cb4c32d",
+    "url": "/semcomp-a11y/static/media/trigenio.f3a7203c.png"
+  },
+  {
+    "revision": "45ee968ac24181f3d327c5df5c6d0d6b",
+    "url": "/semcomp-a11y/static/media/tva_autoridade_de_variancia_do_tempo.45ee968a.png"
+  },
+  {
+    "revision": "1d44347ece8871735e4d0b12f11721a1",
+    "url": "/semcomp-a11y/static/media/ultra_logo.1d44347e.svg"
+  },
+  {
+    "revision": "c0429c3b8dd71e02fa92676f0d18f31f",
+    "url": "/semcomp-a11y/static/media/vai_descer_moto.c0429c3b.png"
+  },
+  {
+    "revision": "95ceec17efa145b0fbed5f4a01e08638",
+    "url": "/semcomp-a11y/static/media/variante.95ceec17.png"
+  }
+]);
